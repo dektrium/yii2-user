87f6e90f8a717e06bca8071e6caa460213b5e86f
Dmitry Erofeev <dmeroff@gmail.com>
2013-11-03 20:56:41 +0400

    refactored User model

--- models/User.php
+++ models/User.php
@@ -24,12 +24,22 @@ class User extends ActiveRecord implements IdentityInterface
     /**
      * The EVENT_BEFORE_REGISTER event occurs before saving the user in the registration process.
      */
-    const EVENT_BEFORE_REGISTER = 'before_registration';
+    const EVENT_BEFORE_REGISTER = 'beforeRegistration';
 
     /**
      * The EVENT_AFTER_REGISTER event occurs after saving the user in the registration process.
      */
-    const EVENT_AFTER_REGISTER = 'after_registration';
+    const EVENT_AFTER_REGISTER = 'afterRegistration';
+
+    /**
+     * The EVENT_BEFORE_LOGIN event occurs before logging the user in.
+     */
+    const EVENT_BEFORE_LOGIN = 'beforeLogin';
+
+    /**
+     * The EVENT_AFTER_LOGIN event occurs before logging the user in.
+     */
+    const EVENT_AFTER_LOGIN = 'afterLogin';
 
     /**
      * @var string Plain password. Used for model validation.
@@ -40,6 +50,11 @@ class User extends ActiveRecord implements IdentityInterface
      * @var bool Whether to remember the user.
      */
     public $rememberMe = false;
+
+    /**
+     * @var User
+     */
+    protected $_identity;
 
     /**
      * @inheritdoc
@@ -65,8 +80,19 @@ class User extends ActiveRecord implements IdentityInterface
             ['username', 'string', 'min' => 3, 'max' => 25],
             ['email', 'string', 'max' => 255],
             ['password', 'string', 'min' => 6],
+            ['password', 'validatePassword', 'on' => ['login']],
             ['rememberMe', 'boolean']
         ];
+    }
+
+    /**
+     * Validates the password.
+     */
+    public function validatePassword()
+    {
+        if ($this->_identity === null || !Security::validatePassword($this->password, $this->_identity->getAttribute('password_hash'))) {
+            $this->addError('password', 'Incorrect email or password.');
+        }
     }
 
     /**
@@ -156,23 +182,20 @@ class User extends ActiveRecord implements IdentityInterface
 
     /**
      * Logs in a user using the provided email and password.
+     *
      * @return boolean whether the user is logged in successfully
      */
     public function login()
     {
+        $this->_identity = self::find(array('email' => $this->getAttribute('email')));
+        $this->trigger(self::EVENT_BEFORE_LOGIN);
         if ($this->validate()) {
-            /** @var User $user */
-            $user = self::find(array('email' => $this->getAttribute('email')));
-            if ($user === null || !Security::validatePassword($this->password, $user->getAttribute('password_hash'))) {
-                $this->addError('password', 'Incorrect email or password.');
-                return false;
-            } else {
-                \Yii::$app->getUser()->login($user, $this->rememberMe ? \Yii::$app->getModule('user')->params['rememberFor'] : 0);
-                \Yii::$app->getSession()->set('user.id', $user->getAttribute('id'));
-                \Yii::$app->getSession()->set('user.username', $user->getAttribute('username'));
-                \Yii::$app->getSession()->set('user.email', $user->getAttribute('email'));
-                return true;
-            }
+            \Yii::$app->getUser()->login($this->_identity, $this->rememberMe ? \Yii::$app->getModule('user')->params['rememberFor'] : 0);
+            \Yii::$app->getSession()->set('user.id', $this->_identity->getAttribute('id'));
+            \Yii::$app->getSession()->set('user.username', $this->_identity->getAttribute('username'));
+            \Yii::$app->getSession()->set('user.email', $this->_identity->getAttribute('email'));
+            $this->trigger(self::EVENT_AFTER_LOGIN);
+            return true;
         }
 
         return false;

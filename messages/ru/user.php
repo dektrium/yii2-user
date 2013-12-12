cb2279b625acea63c1ee59313ae4d62147b81f5d
Dmitry Erofeev <dmeroff@gmail.com>
2013-12-12 18:20:22 +0400

    added russian translation

---
+++ messages/ru/user.php
@@ -0,0 +1,39 @@
+<?php return [
+    'Account has been created' => 'Вы были зарегистрированы',
+    'Awesome! Your account has been created' => 'Вы были зарегистрированы',
+    'Awesome! Your password has been reset' => 'Ваш пароль был сброшен',
+    'Awesome, almost there!' => 'Почти готово!',
+    'Awesome, almost there! We need to confirm your email address' =>
+        'Почти готово! Вам необходимо активировать ваш аккаунт',
+    'Confirmation needed' => 'Необходима активация аккаунта',
+    'Confirmation token is invalid' => 'Сссылка для активации аккаунта устарела',
+    'Didn\'t receive confirmation message?' => 'Не пришло письмо?',
+    'Email' => 'Email',
+    'Forgot password?' => 'Забыли пароль?',
+    'If you\'re having troubles, you can resend it by clicking the link below:' =>
+        'Если писмьмо не пришло, вы можете запросить новое, нажав на ссылку ниже',
+    'Invalid login or password' => 'Неправильный логин или пароль',
+    'Log in' => 'Войти',
+    'Password' => 'Пароль',
+    'Password has been reset' => 'Пароль был сброшен',
+    'Password recovery' => 'Восстановление пароля',
+    'Please check your email and click the confirmation link to complete your registration.' =>
+        'Пожалуйста, проверьте ваш почтовый ящик и нажмите на ссылку, чтобы завершить регистрацию',
+    'Recovery message sent' => 'Письмо со ссылкой для смены пароля было отправлено',
+    'Recovery token is invalid' => 'Сссылка для смены пароля устарела',
+    'Register' => 'Зарегистрироваться',
+    'Remember me next time' => 'Запомнить меня',
+    'Request new confirmation message' => 'Повторная отправка инструкций',
+    'Reset password' => 'Сбросить пароль',
+    'Send' => 'Отправить',
+    'This account has already been confirmed' => 'Этот аккаунт уже был активирован',
+    'Username' => 'Имя пользователя',
+    'We\'re sorry but your confirmation token is invalid. You can request new token by clicking the link below:' =>
+        'К сожалению эта ссылка устарела. Вы можете запросить новую, кликнув по ссылке ниже',
+    'You can log in using your credentials now' => 'Теперь вы можете войти, используя ваши логин и пароль',
+    'You can log in using your new password now' => 'Теперь вы можете войти, используя ваш новый пароль',
+    'You have been sent an email with instructions on how to reset your password.' =>
+        'Вам были отправлены инструкции по смене пароля',
+    'You must confirm your account before logging in' => 'Вы должны сначала активировать ваш аккаунт',
+    'Your account has been confirmed' => 'Ваш аккаунт был активирован',
+];

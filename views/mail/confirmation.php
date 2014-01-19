a26ae057f369eea039084395a5a4352c57831ffb
Dmitry Erofeev <dmeroff@gmail.com>
2014-01-19 16:11:09 +0400

    modified view files

--- views/mail/confirmation.php
+++ views/mail/confirmation.php
@@ -1,16 +1,24 @@
 <?php
+
 use yii\helpers\Html;
 
 /**
- * @var yii\web\View $this
- * @var dektrium\user\models\User $user
+ * @var dektrium\user\models\UserInterface $user
  */
 ?>
-<p>
-	You recently registered for a new account with <?= Yii::$app->name; ?>. Before your account is activated, we
-	need you to confirm your email address.
+<p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
+	<?= Yii::t('user', 'Hi there,')?>
 </p>
-<p>
-	To complete your registration, please click the link below:<br>
-	<?= Html::a($user->getConfirmationUrl(), $user->getConfirmationUrl()); ?>
+<p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
+	<?= Yii::t('user', 'You recently registered for a new account on {sitename}', ['sitename' => Yii::$app->name]) ?>.
+	<?= Yii::t('user', 'Before your account is activated, we need you to confirm your email address') ?>.
+</p>
+<p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
+	<?= Yii::t('user', 'To complete your registration, please click the link below') ?>
+</p>
+<p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
+	<?= Html::a(Html::encode($user->getConfirmationUrl()), $user->getConfirmationUrl()); ?>
+</p>
+<p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
+	<?= Yii::t('user', 'If you have problems, please paste the above URL into your web browser') ?>
 </p>
--- views/mail/recovery.php
+++ views/mail/recovery.php
@@ -1,20 +1,22 @@
 <?php
+
 use yii\helpers\Html;
 
 /**
- * @var yii\web\View $this
- * @var dektrium\user\models\User $user
+ * @var dektrium\user\models\UserInterface $user
  */
 ?>
-<p>
-	You've recently requested to reset your password on <?= Yii::$app->name; ?>.
-
-	In order to complete this request, we need you to verify that you initiated this request. Please click the link
-	below to complete your password reset.
+<p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
+	<?= Yii::t('user', 'Hi there,')?>
 </p>
-<p>
-	<?= Html::a($user->getRecoveryUrl(), $user->getRecoveryUrl()); ?>
+<p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
+	<?= Yii::t('user', 'You have recently requested to reset your password on {sitename}', ['sitename' => Yii::$app->name]) ?>.
+	<?= Yii::t('user', 'In order to complete this request, we need you to verify that you initiated this request') ?>.
+	<?= Yii::t('user', 'Please click the link below to complete your password reset') ?>.
 </p>
-<p>
-	P.S. If you did not request to reset your password, please disregard this message. Your account is safe.
+<p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
+	<?= Html::a(Html::encode($user->getRecoveryUrl()), $user->getRecoveryUrl()); ?>
+</p>
+<p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
+	<?= Yii::t('user', 'P.S. If you did not request to reset your password, please disregard this message. Your account is safe.') ?>
 </p>

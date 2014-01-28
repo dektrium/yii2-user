d9b4685a3f127fef95c5839d07fde6d4e2f591e7
Dmitry Erofeev <dmeroff@gmail.com>
2014-01-28 19:20:55 +0400

    added delete action

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -2,9 +2,12 @@
 
 use dektrium\user\models\UserSearch;
 use yii\web\Controller;
+use yii\web\NotFoundHttpException;
 
 /**
  * AdminController allows you to administrate users.
+ *
+ * @property \dektrium\user\Module $module
  *
  * @author Dmitry Erofeev <dmeroff@gmail.com
  */
@@ -19,5 +22,36 @@ class AdminController extends Controller
 			'dataProvider' => $dataProvider,
 			'searchModel' => $searchModel,
 		]);
+	}
+
+	/**
+	 * Deletes an existing User model.
+	 * If deletion is successful, the browser will be redirected to the 'index' page.
+	 * @param integer $id
+	 * @return mixed
+	 */
+	public function actionDelete($id)
+	{
+		$this->findModel($id)->delete();
+
+		return $this->redirect(['index']);
+	}
+
+	/**
+	 * Finds the User model based on its primary key value.
+	 * If the model is not found, a 404 HTTP exception will be thrown.
+	 * @param integer $id
+	 * @return \dektrium\user\models\User the loaded model
+	 * @throws NotFoundHttpException if the model cannot be found
+	 */
+	protected function findModel($id)
+	{
+		/** @var \dektrium\user\models\User $user */
+		$user = $this->module->factory->createQuery()->where(['id' => $id])->one();
+		if ($id !== null && $user !== null) {
+			return $user;
+		} else {
+			throw new NotFoundHttpException('The requested page does not exist.');
+		}
 	}
 }

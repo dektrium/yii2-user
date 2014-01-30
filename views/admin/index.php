41512821292198e2d7c470ce8e5d6264bb5ffee3
Dmitry Erofeev <dmeroff@gmail.com>
2014-01-30 15:55:06 +0400

    updated grid

--- views/admin/index.php
+++ views/admin/index.php
@@ -35,15 +35,58 @@ $this->params['breadcrumbs'][] = $this->title;
 		'columns' => [
 			'username',
 			'email:email',
-			'create_time:datetime',
-			'registration_ip',
 			[
-				'value' => function ($model) {
+				'attribute' => 'create_time',
+				'label' => 'Registration time',
+				'value' => function ($model, $index, $widget) {
+					return Yii::t('user', '{0, date, MMMM dd, YYYY HH:mm}', [$model->create_time]);
+				}
+			],
+			[
+				'attribute' => 'registration_ip',
+				'value' => function ($model, $index, $widget) {
+					return $model->registration_ip == null ? '<span class="not-set">(not set)</span>' : long2ip($model->registration_ip);
+				},
+				'format' => 'html'
+			],
+			[
+				'value' => function ($model, $index, $widget) {
 					return $model->isConfirmed ? '<span class="text-success">Confirmed</span>' : '<span class="text-warning">Not confirmed</span>';
 				},
 				'format' => 'html'
 			],
-			['class' => 'yii\grid\ActionColumn'],
+			[
+				'class' => 'yii\grid\ActionColumn',
+				'template' => '{confirm} {update} {delete}',
+				'buttons' => [
+					'update' => function ($url, $model) {
+						return Html::a('<i class="glyphicon glyphicon-wrench"></i>', $url, [
+							'class' => 'btn btn-xs btn-info',
+							'title' => Yii::t('yii', 'Update'),
+						]);
+					},
+					'delete' => function ($url, $model) {
+						return Html::a('<i class="glyphicon glyphicon-trash"></i>', $url, [
+							'class' => 'btn btn-xs btn-danger',
+							'data-method' => 'post',
+							'data-confirm' => 'Are you sure to delete this user?',
+							'title' => Yii::t('yii', 'Delete'),
+						]);
+					},
+					'confirm' => function ($url, $model) {
+						$params =  [
+							'class' => 'btn btn-xs btn-success',
+							'data-method' => 'post',
+							'data-confirm' => 'Are you sure to confirm this user?',
+							'title' => Yii::t('user', 'Confirm'),
+						];
+						if ($model->getIsConfirmed()) {
+							$params['disabled'] = 'disabled';
+						}
+						return Html::a('<i class="glyphicon glyphicon-ok"></i>', $url, $params);
+					}
+				]
+			],
 		],
 	]); ?>
 

fc773af145f5417ceac4afb74adac3642a00f94d
Dmitry Erofeev <dmeroff@gmail.com>
2014-02-18 19:40:56 +0400

    added Profile model

---
+++ models/Profile.php
@@ -0,0 +1,83 @@
+<?php
+
+/*
+ * This file is part of the Dektrium project.
+ *
+ * (c) Dektrium project <http://github.com/dektrium/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace dektrium\user\models;
+
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "profile".
+ *
+ * @property integer $user_id
+ * @property string $name
+ * @property string $public_email
+ * @property string $gravatar_email
+ * @property string $gravatar_id
+ * @property string $location
+ * @property string $website
+ * @property string $bio
+ *
+ * @author Dmitry Erofeev <dmeroff@gmail.com
+ */
+class Profile extends ActiveRecord
+{
+	/**
+	 * @inheritdoc
+	 */
+	public static function tableName()
+	{
+		return 'profile';
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function rules()
+	{
+		return [
+			[['user_id'], 'required'],
+			[['user_id'], 'integer'],
+			[['bio'], 'string'],
+			[['name', 'public_email', 'gravatar_email', 'location', 'website'], 'string', 'max' => 255],
+		];
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function attributeLabels()
+	{
+		return [
+			'name' => \Yii::t('user', 'Name'),
+			'public_email' => \Yii::t('user', 'Email (public)'),
+			'gravatar_email' => \Yii::t('user', 'Gravatar email (private)'),
+			'location' => \Yii::t('user', 'Location'),
+			'website' => \Yii::t('user', 'Website'),
+			'bio' => \Yii::t('user', 'Bio'),
+		];
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function beforeSave($insert)
+	{
+		if (parent::beforeSave($insert)) {
+			if ($this->isAttributeChanged('gravatar_email')) {
+				$this->setAttribute('gravatar_id', md5($this->getAttribute('gravatar_email')));
+			}
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}

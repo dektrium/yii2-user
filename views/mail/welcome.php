f2844ada70a590a15090dffe48eb835525353473
Dmitry Erofeev <dmeroff@gmail.com>
2014-03-24 19:41:23 +0400

    refactored email views

---
+++ views/mail/welcome.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the Dektrium project.
+ *
+ * (c) Dektrium project <http://github.com/dektrium>
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+use yii\helpers\Html;
+
+/**
+ * @var dektrium\user\models\User $user
+ */
+?>
+<p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
+    <?= Yii::t('user', 'Hello') ?>,
+</p>
+<p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
+    <?= Yii::t('user', 'Your account on {0} has been successfully created', Yii::$app->name) ?>.
+    <?= Yii::t('user', 'Your username is {0} and your password is {1}', [$user->username, $user->password]) ?>.
+</p>
+<?php if (Yii::$app->getModule('user')->confirmable): ?>
+    <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
+        <?= Yii::t('user', 'In order to complete your registration, please click the link below') ?>:
+    </p>
+    <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
+        <?= Html::a(Html::encode($user->getConfirmationUrl()), $user->getConfirmationUrl()); ?>
+    </p>
+    <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
+        <?= Yii::t('user', 'If you have problems, please paste the above URL into your web browser') ?>.
+        <?= Yii::t('user', 'This URL will only be valid for a limited time and will expire') ?>.
+    </p>
+<?php endif; ?>
+<p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
+    <?= Yii::t('user', 'P.S. If you received this email by mistake, simply delete it') ?>.
+</p>

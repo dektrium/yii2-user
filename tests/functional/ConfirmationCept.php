24edb5ded1217232ce318835bbe7d806d85f5e45
Dmitry Erofeev <dmeroff@gmail.com>
2014-03-26 20:00:17 +0400

    updated confirmation cept

--- tests/functional/ConfirmationCept.php
+++ tests/functional/ConfirmationCept.php
@@ -1,16 +1,22 @@
 <?php
 
+use yii\helpers\Url;
+use dektrium\user\models\User;
+
 $I = new TestGuy($scenario);
 $I->wantTo('ensure that confirmation works');
 
 $I->amGoingTo('check that error is showed when token expired');
-$I->amOnPage('?r=user/registration/confirm&id=3&token=qxYa315rqRgCOjYGk82GFHMEAV3T82AX');
+$user = $I->getFixture('user')->getModel('unconfirmed_with_expired_token');
+$I->amOnPage(Url::toRoute(['/user/registration/confirm', 'id' => $user->id, 'token' => $user->confirmation_token]));
 $I->see('Confirmation token is invalid');
 
 $I->amGoingTo('check that user get confirmed');
-$I->amOnPage('?r=user/registration/confirm&id=2&token=NO2aCmBIjFQX624xmAc3VBu7Th3NJoa6');
+$user = $I->getFixture('user')->getModel('unconfirmed');
+$I->amOnPage(Url::toRoute(['/user/registration/confirm', 'id' => $user->id, 'token' => $user->confirmation_token]));
 $I->see('Your account has been confirmed');
-$I->grabRecord('\dektrium\user\models\User', [
-    'id' => 2,
+$I->haveRecord(User::className(), [
+    'id' => $user->id,
     'confirmation_token' => null,
+    'confirmation_sent_at' => null
 ]);

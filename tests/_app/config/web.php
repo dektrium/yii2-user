da1384c9743a6d120ae05d6f2db72ff70008c4bb
Dmitry Erofeev <dmeroff@gmail.com>
2014-04-03 17:00:40 +0400

    added missed assets folder, fixed travis.yml

--- tests/_app/config/web.php
+++ tests/_app/config/web.php
@@ -20,7 +20,7 @@ return [
     ],
     'components' => [
         'assetManager' => [
-            'basePath' => '@tests/_app/runtime/assets'
+            'basePath' => '@tests/_app/assets'
         ],
         'log'   => null,
         'cache' => null,

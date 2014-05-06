00c3193c9338c070c3cf895f71b0879ad12c41e0
Dmitry Erofeev <dmeroff@gmail.com>
2014-05-06 12:29:04 +0400

    updated Mailer

--- Mailer.php
+++ Mailer.php
@@ -11,6 +11,7 @@
 
 namespace dektrium\user;
 
+use dektrium\user\models\Token;
 use dektrium\user\models\User;
 use yii\base\Component;
 
@@ -34,60 +35,64 @@ class Mailer extends Component
     /**
      * Sends an email to a user with credentials and confirmation link.
      *
-     * @param  User $user
+     * @param  User  $user
+     * @param  Token $token
      * @return bool
      */
-    public function sendWelcomeMessage(User $user)
+    public function sendWelcomeMessage(User $user, Token $token = null)
     {
-        return $this->sendMessage($user->email, \Yii::t('user', 'Welcome to {0}', \Yii::$app->name), 'welcome', $user);
+        return $this->sendMessage($user->email,
+            \Yii::t('user', 'Welcome to {0}', \Yii::$app->name),
+            'welcome',
+            ['user' => $user, 'token' => $token]
+        );
     }
 
     /**
      * Sends an email to a user with confirmation link.
      *
      * @param  User $user
+     * @param  Token $token
      * @return bool
      */
-    public function sendConfirmationMessage(User $user)
+    public function sendConfirmationMessage(User $user, Token $token)
     {
-        return $this->sendMessage($user->email, \Yii::t('user', 'Confirm your account on {0}', \Yii::$app->name), 'confirmation', $user);
-    }
-
-    /**
-     * Sends an email to a user with reconfirmation link.
-     *
-     * @param  User $user
-     * @return bool
-     */
-    public function sendReconfirmationMessage(User $user)
-    {
-        return $this->sendMessage($user->unconfirmed_email, \Yii::t('user', 'Confirm your email change on {0}', \Yii::$app->name), 'reconfirmation', $user);
+        return $this->sendMessage($user->email,
+            \Yii::t('user', 'Confirm your account on {0}', \Yii::$app->name),
+            'confirmation',
+            ['user' => $user, 'token' => $token]
+        );
     }
 
     /**
      * Sends an email to a user with recovery link.
      *
      * @param  User $user
+     * @param  Token $token
      * @return bool
      */
-    public function sendRecoveryMessage(User $user)
+    public function sendRecoveryMessage(User $user, Token $token)
     {
-        return $this->sendMessage($user->email, \Yii::t('user', 'Complete your password reset on {0}', \Yii::$app->name), 'recovery', $user);
+        return $this->sendMessage($user->email,
+            \Yii::t('user', 'Complete your password reset on {0}', \Yii::$app->name),
+            'recovery',
+            ['user' => $user, 'token' => $token]
+        );
     }
 
     /**
      * @param  string $to
      * @param  string $subject
      * @param  string $view
-     * @param  User   $user
+     * @param  array  $params
      * @return bool
      */
-    protected function sendMessage($to, $subject, $view, User $user)
+    protected function sendMessage($to, $subject, $view, $params = [])
     {
         $mail = \Yii::$app->mail;
         $mail->viewPath = $this->viewPath;
 
-        return $mail->compose($view, ['user' => $user])
+        return $mail->compose($view, $params)
             ->setTo($to)
             ->setFrom($this->sender)
             ->setSubject($subject)

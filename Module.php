15b37984ae9fb74153ac1d660c2edeb0049c62f6
Dmitry Erofeev <dmeroff@gmail.com>
2014-08-31 16:57:48 +0400

    bumped version number

--- Module.php
+++ Module.php
@@ -23,7 +23,7 @@ use yii\base\Module as BaseModule;
  */
 class Module extends BaseModule
 {
-    const VERSION = '0.7.0-dev';
+    const VERSION = '0.7.0';
 
     /**
      * @var string Web user class.

c9a3a25c09c8cace9692de825661f6df7cbebfef
Dmitry Erofeev <dmeroff@gmail.com>
2014-10-11 11:32:03 +0400

    refactored ProfileController

--- controllers/ProfileController.php
+++ controllers/ProfileController.php
@@ -11,6 +11,7 @@
 
 namespace dektrium\user\controllers;
 
+use dektrium\user\Finder;
 use yii\web\Controller;
 use yii\filters\AccessControl;
 use yii\web\NotFoundHttpException;
@@ -24,25 +25,30 @@ use yii\web\NotFoundHttpException;
  */
 class ProfileController extends Controller
 {
+    /** @var Finder */
+    protected $finder;
+
     /**
-     * @inheritdoc
+     * @param string $id
+     * @param \yii\base\Module $module
+     * @param Finder $finder
+     * @param array $config
      */
+    public function __construct($id, $module, Finder $finder, $config = [])
+    {
+        $this->finder = $finder;
+        parent::__construct($id, $module, $config);
+    }
+
+    /** @inheritdoc */
     public function behaviors()
     {
         return [
             'access' => [
                 'class' => AccessControl::className(),
                 'rules' => [
-                    [
-                        'allow' => true,
-                        'actions' => ['index'],
-                        'roles' => ['@']
-                    ],
-                    [
-                        'allow' => true,
-                        'actions' => ['show'],
-                        'roles' => ['?', '@']
-                    ],
+                    ['allow' => true, 'actions' => ['index'], 'roles' => ['@']],
+                    ['allow' => true, 'actions' => ['show'], 'roles' => ['?', '@']],
                 ]
             ],
         ];
@@ -50,32 +56,29 @@ class ProfileController extends Controller
 
     /**
      * Redirects to current user's profile.
-     *
      * @return \yii\web\Response
      */
     public function actionIndex()
     {
-        return $this->redirect(['show', 'id' => \Yii::$app->getUser()->getId()]);
+        return $this->redirect(['show', 'id' => \Yii::$app->user->getId()]);
     }
 
     /**
      * Shows user's profile.
-     *
-     * @param $id
-     *
+     * @param  integer $id
      * @return \yii\web\Response
      * @throws \yii\web\NotFoundHttpException
      */
     public function actionShow($id)
     {
-        $profile = $this->module->manager->findProfileById($id);
+        $profile = $this->finder->findProfileById($id);
 
         if ($profile === null) {
             throw new NotFoundHttpException;
         }
 
         return $this->render('show', [
-            'profile' => $profile
+            'profile' => $profile,
         ]);
     }
 }

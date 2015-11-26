de3a4fa277deabc3114b4056e2411ce3770ede08
bscheshirwork <bscheshir.work@gmail.com>
2015-11-26 15:39:30 +0300

    fix double-click selection within spaces before
    Double click on the password in the text of the letter, highlights the extra space.
    " h5ErMcP2" -> "h5ErMcP2"

--- views/mail/welcome.php
+++ views/mail/welcome.php
@@ -26,8 +26,7 @@ use yii\helpers\Html;
 <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
     <?= Yii::t('user', 'Your account on {0} has been created', Yii::$app->name) ?>.
     <?php if ($showPassword || $module->enableGeneratingPassword): ?>
-        <?= Yii::t('user', 'We have generated a password for you') ?>:
-        <strong><?= $user->password ?></strong>
+        <?= Yii::t('user', 'We have generated a password for you') ?>: <strong><?= $user->password ?></strong>
     <?php endif ?>
 
 </p>

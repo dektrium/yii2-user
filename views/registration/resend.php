5c30ad2c4f00ad0ae24765cc1ad5ea0ee0fec85d
Bulat Damdinov <dbulats88@gmail.com>
2016-04-04 12:18:56 +0800

    change column form for tablet devices

--- views/registration/resend.php
+++ views/registration/resend.php
@@ -21,7 +21,7 @@ $this->title = Yii::t('user', 'Request new confirmation message');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="row">
-    <div class="col-md-4 col-md-offset-4">
+    <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
         <div class="panel panel-default">
             <div class="panel-heading">
                 <h3 class="panel-title"><?= Html::encode($this->title) ?></h3>

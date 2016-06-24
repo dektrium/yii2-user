69a16c37360ec3cef6eb308948c1e1c4f10f191c
Dmitry Erofeev <dmeroff@yandex.ru>
2016-06-24 15:30:58 +0300

    Fix #708

--- views/mail/welcome.php
+++ views/mail/welcome.php
@@ -40,8 +40,8 @@ use yii\helpers\Html;
 </p>
 <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
     <?= Yii::t('user', 'If you cannot click the link, please try pasting the text into your browser') ?>.
-    <?php endif ?>
 </p>
+<?php endif ?>
 
 <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
     <?= Yii::t('user', 'If you did not make this request you can ignore this email') ?>.

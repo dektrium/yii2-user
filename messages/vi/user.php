57f3b62c4651e22e368d7ad39e0b7b432746cdea
Le Phuong <phuong17889@gmail.com>
2016-10-15 16:23:30 +0700

    follow @Thoulah

--- messages/vi/user.php
+++ messages/vi/user.php
@@ -10,7 +10,7 @@
  * Each array element represents the translation (value) of a message (key).
  * If the value is empty, the message is considered as not translated.
  * Messages that no longer need translation will have their translations
- * enclosed between a pair of '' marks.
+ * enclosed between a pair of '@@' marks.
  *
  * Message string can be used with plural forms format. Check i18n section
  * of the guide for details.

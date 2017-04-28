4165d8cb4e6faf02edd52154635490bc2c18cbf8
Andrew Oblikov <andrew.rbe@gmail.com>
2017-04-28 23:17:19 +0300

    Not all timezones have hour offset, so we can not just divide offset in seconds to 3600 Examples: Pacific/Chatham (UTC +12:45); Asia/Kathmandu (UTC +05:45)

--- helpers/Timezone.php
+++ helpers/Timezone.php
@@ -31,10 +31,11 @@ class Timezone
 
         foreach ($timeZoneIdentifiers as $timeZone) {
             $date = new \DateTime('now', new \DateTimeZone($timeZone));
-            $offset = $date->getOffset() / 60 / 60;
+            $offset = $date->getOffset();
+            $tz = ($offset > 0 ? '+' : '-') . gmdate('H:i', abs($offset));
             $timeZones[] = [
                 'timezone' => $timeZone,
-                'name' => "{$timeZone} (UTC " . ($offset > 0 ? '+' : '') . "{$offset})",
+                'name' => "{$timeZone} (UTC {$tz})",
                 'offset' => $offset
             ];
         }

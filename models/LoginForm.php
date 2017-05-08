a6a47823ec07eca52c3acb1fcb615b89f6c7e9a8
Thiago Talma <thiagotalma@users.noreply.github.com>
2017-05-08 17:53:49 -0300

    Update LoginForm.php

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -64,7 +64,9 @@ class LoginForm extends Model
         /** @var \dektrium\user\Module $module */
         $module = \Yii::$app->getModule('user');
 
-        return ArrayHelper::map($module->modelMap['User']::find()->where(['blocked_at' => null])->all(), 'username', function ($user) {
+        $user = $module->modelMap['User'];
+
+        return ArrayHelper::map($user::find()->where(['blocked_at' => null])->all(), 'username', function ($user) {
             return sprintf('%s (%s)', Html::encode($user->username), Html::encode($user->email));
         });
     }

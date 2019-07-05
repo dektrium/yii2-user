b11d1e083ed18c5eed4785453d7dc37ef0998c43
maranqz <ilia.sergunin@gmail.com>
2019-07-05 18:18:34 +0300

    Fix translate

--- views/mail/recovery-codes.php
+++ views/mail/recovery-codes.php
@@ -32,7 +32,7 @@ $helper = Instance::ensure(RecoveryCodesHelper::class);
     <?= Yii::t('user', 'You have enabled two-factor authentication') ?>.
 </p>
 <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
-    <?= Yii::t('user', 'Please save recovery codes.') ?>.
+    <?= Yii::t('user', 'Please save recovery codes') ?>.
 </p>
 <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
     <?= Yii::t('user',

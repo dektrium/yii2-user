ad3a1035aaf98ecc951718a37046fba347abf5d4
rajdeepr23 <42973627+rajdeepr23@users.noreply.github.com>
2020-03-04 18:32:24 +0530

    Php intl error on /user/show/<:id>
    fixed profile show error when extension not loaded

--- views/profile/show.php
+++ views/profile/show.php
@@ -47,7 +47,14 @@ $this->params['breadcrumbs'][] = $this->title;
                         </li>
                     <?php endif; ?>
                     <li>
-                        <i class="glyphicon glyphicon-time text-muted"></i> <?= Yii::t('user', 'Joined on {0, date}', $profile->user->created_at) ?>
+                        <i class="glyphicon glyphicon-time text-muted"></i> 
+                        <?php
+                        if (extension_loaded('intl')) {
+                            echo Yii::t('user', 'Joined on {0, date}', $profile->user->created_at);
+                        } else {
+                            echo "Joined on ".date('Y-m-d G:i:s', $profile->user->created_at);
+                        }
+                        ?>
                     </li>
                 </ul>
                 <?php if (!empty($profile->bio)): ?>
